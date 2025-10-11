7c0619297ecae9ee899fcda8a99d613b6ddab2d1
Victor <vcovictor01@gmail.com>
2025-10-11 18:55:13 -0300

    Code for histogram and boxplot

---
+++ homework1/hist1.R
@@ -0,0 +1,24 @@
+
+#Creating vector with emission values
+emissions <- c(15.8, 22.7, 26.8, 19.1, 18.5, 14.4, 8.3, 25.9, 26.4, 9.8, 21.9, 10.5, 17.3,
+        6.2, 18.0, 22.9, 24.6, 19.4, 12.3, 15.9, 20.1, 17.0, 22.3, 27.5, 23.9, 17.5, 11.0,
+        20.4, 16.2, 20.8, 20.9, 21.4, 18.0, 24.3, 11.8, 17.9, 18.7, 12.8, 15.5, 19.2, 13.9,
+        28.6, 19.4, 21.6, 13.5, 24.6, 20.0, 24.1, 9.0, 17.6, 25.7, 20.1, 13.2, 23.7, 10.7,
+        19.0, 14.5, 18.1, 31.8, 28.5, 22.7, 15.2, 23.0, 29.6, 11.2, 14.7, 20.5, 26.6, 13.3,
+        18.1, 24.8, 26.1, 7.7, 22.5, 19.3, 19.4, 16.7, 16.9, 23.5, 18.4)
+
+#Plotting the histogram
+hist(emissions, 
+        main = "Emissions Histogram",
+        xlab = "Emissions per class")
+
+boxplot(emissions,
+        main = "Emissions Boxplot",
+        ylab = "Emissions")
+
+text(x=1.1, y = 20.3, 
+        labels = paste("Median = 19.15"))
+text(x=1.1, y = 15.4, 
+        labels = paste("Quartile 1"))
+text(x=1.1, y = 23.05, 
+        labels = paste("Quartile 3"))
